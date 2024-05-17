e444c4f0edab018e92b064580322af658e4053c5
sarojchauhan28 <saroj0007@gmail.com>
2024-05-17 13:44:26 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "Saroj"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInnumber = Number(score)
+// console.log(typeof valueInnumber)
+// console.log(valueInnumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "saroj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Saroj" => true
+
+
+let someNumber = 55
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
