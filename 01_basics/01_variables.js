b57f6301f30994a16da2d0db1df62016df30b1c5
sarojchauhan28 <saroj0007@gmail.com>
2024-05-17 12:34:49 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 11232
+let accountEmail = "saroj@gmail.com"
+var accountPassword = "123456"
+accountCity = "Ghaziabad" // not a good idea to use without variable
+
+let accountState; // it will print undefined
+
+// accountId = 2 // not allowed
+accountEmail = "parul@gmail.com"
+accountPassword = "909056"
+accountCity = "delhi"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version
+
+// alert( "2 + 2" ) // we are using node js, not browser
+
+console.log(3 
+    + 
+    3) //code readability should be high
+console.log("saroj")
+
+
+// 2 most iportant documention
+// https://tc39.es/ecma262/#sec-global-object
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+
+let name = "Saroj"
+let age = 37
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string  => ""
+// boolean => True/false
+// null => standalone value
+// undefined => 
+// symobol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null) //object
+
