8e7a380825ab3f649ee1cabd1e6b2f1b6300e821
sarojchauhan28 <saroj0007@gmail.com>
2024-05-22 13:40:22 +0000

    array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,10 @@
+const marvel_heros = ["spiderman", "thor", "woderwoman" ]
+const dc_heros = ["superman", "flash", "batman" ]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][2]);
+
+const all_heros = marvel_heros.concat(dc_heros)
+console.log(all_heros);
