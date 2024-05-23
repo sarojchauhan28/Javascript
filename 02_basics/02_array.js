1807e3677f3634de0a04342e89abf737428075dd
sarojchauhan28 <saroj0007@gmail.com>
2024-05-23 06:59:41 +0000

    array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -6,5 +6,25 @@ const dc_heros = ["superman", "flash", "batman" ]
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][2]);
 
-const all_heros = marvel_heros.concat(dc_heros)
-console.log(all_heros);
+// const all_heros = marvel_heros.concat(dc_heros)
+// console.log(all_heros);
+
+const allNewHeros = [...marvel_heros, ...dc_heros]
+// console.log(allNewHeros);
+
+const anotherArray = [1, 2, [ 3, 4,], 5, [6, 7,[ 8, 9]]]
+
+const realAnotherArray = anotherArray.flat(Infinity)
+// console.log(realAnotherArray);
+
+console.log(Array.isArray('Saroj'));
+console.log(Array.from('Saroj'));
+console.log(Array.from({name : 'Saroj'})); // interesting case 
+
+let score1 = 100
+let score2 = 400
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+//need to more study on array  is, from and of 
