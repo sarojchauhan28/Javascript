c0e94bee0ef481fe75e388670e2ef99b759a26a6
sarojchauhan28 <saroj0007@gmail.com>
2024-09-25 10:56:31 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,11 +5,47 @@ let a = 400
 if (true){
     let a = 10
     const b = 20
-    console.log('inner:', a);
+    // console.log('inner:', a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
-// console.log(c); 
+// console.log(c);
 
+
+function one(){
+    const username = "Saroj"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website)
+    two();
+}
+// one();
+
+if(true){
+    const username = "Saroj"
+    if(username ==="Saroj"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// +++++++++++++ intresting ++++++++++++++
+
+addOne(5)
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+
+}
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
