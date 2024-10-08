1088933971dfb34f2186d5b6e7f27526cf67b085
sarojchauhan28 <saroj0007@gmail.com>
2024-10-08 08:29:08 +0000

    truthy

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "jan"
+switch (month) {
+    case "jan":
+        console.log('january');
+        break;
+    case "Feb":
+        console.log('Feb');
+        
+        break;
+    case "March":
+        console.log('March');
+        
+        // break;
+    case "April":
+        console.log('April');
+        
+        break;
+
+    default:
+        console.log('default case match');
+        break;
+}
