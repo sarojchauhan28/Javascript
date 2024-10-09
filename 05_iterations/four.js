be62cc51ef97c326711ba6c8b836608a8e59ff2e
sarojchauhan28 <saroj0007@gmail.com>
2024-10-09 13:06:38 +0000

    iterations

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    Js : "javascript",
+    CPP : "C++",
+    rb : "Ruby",
+    swift : "Swift by Apple",
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+       
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United State of America")
+// map.set('Fr', "Franc")
+// map.set('IN', "India")
+
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
