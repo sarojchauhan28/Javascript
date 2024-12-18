680f7319f99fcb6f4aa05b8e0e32748d02062342
sarojchauhan28 <saroj0007@gmail.com>
2024-12-18 12:34:02 +0000

    promises

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,86 @@
+const promiseOne = new Promise((resolve, reject) => {
+    //Do an async task
+    //DB calls, cryptography, network
+    setTimeout(() => {
+        console.log('Async task is complete');
+        resolve()
+        
+    }, 1000);
+})
+
+promiseOne.then(function(){
+    console.log('promis consumed')
+})
+
+new Promise((resolve,reject) => {
+    setTimeout(() => {
+        console.log('Async task 2');
+        resolve()
+    }, 1000)
+}).then(function(){
+    console.log("Async is resolved")
+})
+
+
+const promiseThree = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({
+            username:"Saroj",
+            email:"saroj@example.com"
+        })
+    }, 1000);
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+    
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        let error = true
+       if (!error) {
+        resolve({
+            username: "sarojchauan",
+            password: "123"
+        })
+       }else{
+            reject('ERROR: Somthing went wrong')        
+       }
+    }, 1000);
+})
+
+promiseFour.then(function(user){
+    console.log(user);
+    return user.username
+}).then(function(username){
+    console.log(username);
+}).catch( (error) => {
+    console.log(error);
+}).finally(()=> console.log("The promise is either resolved or rejected "))
+
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        let error = false
+       if (!error) {
+        resolve({
+            username: "javascript",
+            password: "123"
+        })
+       }else{
+            reject('ERROR: JS went wrong')        
+       }
+    }, 1000);
+})
+
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+consumePromiseFive()
