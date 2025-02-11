1c467191f758d86228df4a0b710279135eb502be
sarojchauhan28 <saroj0007@gmail.com>
2025-02-11 06:52:55 +0000

    MathPI

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,38 @@
+const discripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(discripter);
+
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+const chai = {
+    name:"ginger tea",
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nahi bani");
+
+    }
+}
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+
+Object.defineProperty(chai,"name",{
+    // writable: false,
+    enumerable: true
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+
+for (let [key, value] of Object.entries(chai)) {
+    
+    if(typeof value !== 'function'){
+
+    console.log(`${key} : ${value} `);
+    }
+    
+}
